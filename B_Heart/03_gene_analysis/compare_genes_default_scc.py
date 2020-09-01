575403943effd4fef4083913bb97fc85523c1c13
Tanya Phung <tanya.n.phung@gmail.com>
2020-09-01 09:39:11 -0400

    add scripts for gene analysis

---
+++ B_Heart/03_gene_analysis/compare_genes_default_scc.py
@@ -0,0 +1,19 @@
+import sys
+
+default_genes = set()
+with open(sys.argv[1], "r") as f:
+    for line in f:
+        items = line.rstrip("\n").split("\t")
+        if int(items[12]) > 10:
+            default_genes.add(items[3])
+
+scc_genes = set()
+with open(sys.argv[2], "r") as f:
+    for line in f:
+        items = line.rstrip("\n").split("\t")
+        if int(items[12]) > 10:
+            scc_genes.add(items[3])
+
+print(len(default_genes.intersection(scc_genes)))
+print(len(default_genes-scc_genes))
+print(len(scc_genes-default_genes))
